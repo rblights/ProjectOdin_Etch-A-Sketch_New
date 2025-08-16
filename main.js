63c2c96e4c106c56fa1f51d6ca0431d202a31afd
B <52431228+rblights@users.noreply.github.com>
2025-08-16 10:09:19 -0700

    Refactored into Sketchpad View

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-import { Sketchpad_M } from "./Model/Sketchpad_M.js";
 import { SliderBar_M } from "./Model/SliderBar_M.js";
+import { Sketchpad_M } from "./Model/Sketchpad_M.js";
+import { Sketchpad_V } from "./View/Sketchpad_V.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     const SLIDERBAR_MODEL = new SliderBar_M();
-    const SKETCHPAD_MODEL = new Sketchpad_M(SLIDERBAR_MODEL);
+    const SKETCHPAD_MODEL = new Sketchpad_M();
+    const SKETCHPAD_VIEW = new Sketchpad_V(SLIDERBAR_MODEL, SKETCHPAD_MODEL);
 })
